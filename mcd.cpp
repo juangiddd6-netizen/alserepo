31c379b97ecce4f48c422bb3a0bd133e0d4ee69b
Juan David <juangidd@outlook.es>
2026-02-18 17:25:53 -0500

    Agregando ejercicio MCD

---
+++ mcd.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num1, num2;
+
+    cout << "Digite el primer numero: ";
+    cin >> num1;
+
+    cout << "Digite el segundo numero: ";
+    cin >> num2;
+
+    int residuo;
+
+    while (num2 != 0) {
+        residuo = num1 % num2;
+        num1 = num2;
+        num2 = residuo;
+    }
+
+    cout << "El MCD es: " << num1;
+
+    return 0;
+}
